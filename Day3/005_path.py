498d2aec8e99c14a20db5f952ec87d16b0be6d5c
niktigbar <nikhil.tigbar@gmail.com>
2022-06-07 17:54:47 +0530

    added best solution

--- Day3/005_path.py
+++ Day3/005_path.py
@@ -17,3 +17,20 @@ def minSumPath(grid):
 
 # follow-up
 # try to solve using real dp approach
+
+
+# I didn't comeup with this solution, but it is best beginners solution ever
+class Solution:
+    def minPathSum(self, grid):
+        n = len(grid)
+        m = len(grid[0])
+        for i in range(1,n):
+            grid[i][0] = grid[i-1][0] + grid[i][0]
+        for j in range(1, m):
+            grid[0][j] = grid[0][j-1] + grid[0][j]
+        
+        for i in range(1,n):
+            for j in range(1, m):
+                grid[i][j] = grid[i][j] + min(grid[i-1][j], grid[i][j-1])
+        return grid[n-1][m-1]
+                
