81e3b730d9815efbb42f742e9f6036d749cb556e
faltunik <niksanat8@gmail.com>
2022-06-11 17:58:18 +0530

    corrected dp soliton

--- Day7/006_subsetsum.py
+++ Day7/006_subsetsum.py
@@ -36,6 +36,8 @@ def subsetSumToK(n, k, arr):
     # k = weight
     # arr  = arr
     # so we can break it into subproblems, every subproblem will have 
+
+    # wrong ans wer given because in 53 line, I wrote i insteaf of i
     
     dp = [[-1 for i in range(k+1)] for i in range(n+1)]
     # if k ==0
@@ -48,7 +50,7 @@ def subsetSumToK(n, k, arr):
     for i in range(1, n+1):
         for j in range(1,k+1):
             if arr[i-1] <=j:
-                dp[i][j] = dp[i-1][j] or dp[i][j-arr[i-1]]
+                dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]]
             else:
                 dp[i][j] = dp[i-1][j] 
     return dp[n][k]
