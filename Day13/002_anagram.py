359419a4f9594ee357ece04cc9f1d991d739c1bf
faltunik <niksanat8@gmail.com>
2022-06-19 16:16:41 +0530

    solved second easy problem of the day

---
+++ Day13/002_anagram.py
@@ -0,0 +1,6 @@
+def areAnagram(str1, str2):
+    # Write your code here.
+    return sorted(str1) == sorted(str2)
+
+# what is the purpose of this questions?
+# https://www.codingninjas.com/codestudio/problems/check-permutation_1172164?topList=striver-sde-sheet-problems
